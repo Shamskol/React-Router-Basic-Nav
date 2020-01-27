d8747085f0c441a17ee13bae09c1ae11ee3d33ee
Kolade-Junaid <koladejun@yahoo.com>
2020-01-27 15:39:33 +0000

    Modified the modified  src/App.js, src/components/Navigation.js and,  src/index.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import React from 'react';
-import './App.css';
-import { Home, About, Contact, Navigation } from './components';
+import React from "react";
+import "./App.css";
+import { Route, Switch, Link } from 'react-router-dom';
+import { Home, About, Contact, Navigation } from "./components";
 
-const App = () => (
-  <div>
-    <Navigation />
-  </div>
-);
+function App() {
+  return (
+    <div className="App">
+      <Navigation>
+<Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
 
+      <Route exact path="/about">
+        <About />
+      </Route>
+
+      <Route exact path="/contact">
+        <Contact />
+      </Route>
+      </Switch>
+      </Navigation>
+    
+    </div>
+
+  );
+}
 export default App;
