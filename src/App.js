907e9b7a152ed36882aa56807bcc9dd73e63eaf4
Kolade-Junaid <koladejun@yahoo.com>
2020-01-28 10:27:56 +0000

    Modified the App.js and Navigation.js files

--- src/App.js
+++ src/App.js
@@ -1,29 +1,28 @@
 import React from "react";
 import "./App.css";
-import { Route, Switch, Link } from 'react-router-dom';
+import { Route, Switch, Link } from "react-router-dom";
 import { Home, About, Contact, Navigation } from "./components";
 
 function App() {
   return (
     <div className="App">
-      <Navigation>
-<Switch>
-      <Route exact path="/">
-        <Home />
-      </Route>
+      <Navigation/>
+        <Route exact path="/">
+          <Home />
+        </Route>
 
-      <Route exact path="/about">
-        <About />
-      </Route>
+        <Route exact path="/about">
+          <About />
+        </Route>
 
-      <Route exact path="/contact">
-        <Contact />
-      </Route>
-      </Switch>
-      </Navigation>
-    
+        <Route exact path="/contact">
+          <Contact />
+        </Route>
+        <Switch>
+          
+        </Switch>
+      
     </div>
-
   );
 }
 export default App;
