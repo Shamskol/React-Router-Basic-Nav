af869fea537b67ad069f19962d4d5780e43e93b7
Kolade-Junaid <koladejun@yahoo.com>
2020-02-24 20:57:00 +0000

    Completed the initial project set up  and modified the Navigation.js, App.js and index.js files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import React from 'react';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
+import { Route, Switch } from 'react-router-dom'
 
-const App = () => (
-  <div>
+const App = () => {
+  return (
+
+<div className="App">
     <Navigation />
+<Switch>
+    <Route exact path="/">
+          <Home />
+        </Route>
+
+        <Route   exact path="/about">
+          <About />
+        </Route>
+
+        <Route exact path="/contact">
+          <Contact />
+        </Route>
+        </Switch>
+
   </div>
+  
 );
+
+  }
 
 export default App;
